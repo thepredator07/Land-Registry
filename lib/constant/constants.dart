ff1d84614937b68eafdaf183c2e07a20ab5de425
thepredator07 <39725485+thepredator07@users.noreply.github.com>
2023-08-19 03:57:15 +0530

    Add files via upload

---
+++ lib/constant/constants.dart
@@ -0,0 +1,10 @@
+String mapBoxApiKey =
+    'pk.eyJ1Ijoic2F1cmFiaG13IiwiYSI6ImNreTRiYzNidjBhMTkydnB2dmpoeGt4ZmgifQ.2QZ4CsNiygDTAhkqASpbPg';
+
+String nftStorageApiKey =
+    'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiJkaWQ6ZXRocjoweGUxOTNEQTkzNkVlOTY5NDAwMTAwQzQ1ZDY0Njc3NDg4YTdiMEIzNEUiLCJpc3MiOiJuZnQtc3RvcmFnZSIsImlhdCI6MTY3NTg2MTA5NTMzNywibmFtZSI6ImxhbmRfcmVnaXN0cnkifQ.g0T220IsQq3x9r8djLDaEXjObRPuZrdc6eDTx16KSvM';
+const String rpcUrl ="https://rpc-mumbai.maticvigil.com/v1/fb27739499dea77417637a89dc9fee9bfb39008b";// "http://127.0.0.1:7545";
+
+const String contractAddress = "0xc4526c7A89115a5F493eC1189a7aDE8bc012189E";//"0xaB2F56BaC33C23FF8dA81A1034ad357571b9D1Fb";
+
+const int chainId = 80001; //Polygon testnet-80001, local ganache-1337
